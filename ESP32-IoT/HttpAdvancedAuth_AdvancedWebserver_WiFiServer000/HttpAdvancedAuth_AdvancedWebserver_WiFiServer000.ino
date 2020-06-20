e8778dc5dec26e0b1ffc21e75083c6a8f9d4f877
dsyleixa <29887867+dsyleixa@users.noreply.github.com>
2020-06-20 16:30:49 +0200

    Add files via upload

---
+++ ESP32-IoT/HttpAdvancedAuth_AdvancedWebserver_WiFiServer000/HttpAdvancedAuth_AdvancedWebserver_WiFiServer000.ino
@@ -0,0 +1,191 @@
+/*
+  HTTP Advanced Authentication example
+  plus AdvancedWebserver example, unified
+  Created Mar 16, 2017 by Ahmed El-Sharnoby.
+  This example code is in the public domain.
+  Reworked by dsyleixa 2019
+*/
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <ESPmDNS.h>
+#include <ArduinoOTA.h>
+#include <WebServer.h>
+#include <WiFiServer.h>
+
+#define wwwport 8008
+#define lanport 8081
+
+WebServer  webserver(wwwport);   // router http port www
+WiFiServer wifiserver(lanport);  // router wifi port lan 
+
+
+char ssid[64]     = "WLAN";
+char password[64] = "18658";
+/*
+char* ssid[64]     = "****ssid****";
+char* password[64] = "****pswd****";
+*/
+char www_username[64] = "admin";
+char www_password[64] = "esp32";
+
+
+#define     this_ip_4    202      // <<< static webserver  ip (4th byte)
+
+IPAddress   this_ip(192, 168, 2, this_ip_4); // <<< static local IP of this ESP-webserver
+IPAddress   gateway(192, 168, 2, 1);         // <<< LAN Gateway IP
+IPAddress   subnet(255, 255, 255, 0);        // <<< LAN Subnet Mask
+char url[128] = "";
+
+
+// allows you to set the realm of authentication Default:"Login Required"
+const char* www_realm = "Custom Auth Realm";
+// the Content of the HTML response in case of Unautherized Access Default:empty
+String authFailResponse = "Authentication Failed";
+
+
+
+//----------------------------------------------------------
+// GPIOs
+char led = LED_BUILTIN;
+
+
+
+
+//----------------------------------------------------------
+void handleRoot() {
+  
+  char temp[255];  
+  String script;
+  
+  int sec = millis() / 1000;
+  int min = sec / 60;
+  int hr = min / 60;
+
+  // time-cstr
+  sprintf(temp,"%02d:%02d:%02d", hr, min%60, sec%60);
+  
+  digitalWrite(led, HIGH);
+
+  script=
+     (String)"<html>" +
+     "<head>" +
+       "<meta http-equiv='refresh' content='5'; charset=utf-8; URL="+url+"/>" +
+       "<title>ESP32 Demo</title>" +
+       "<style>" +
+         "body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }" +       
+         "</style>" +
+     "</head>" +
+     "<body>" +
+       "<h1>Hello from ESP32 </h1>" + url + (String)" °!°" + // debug: ° <> utf-8 test!
+       "<p>Uptime:" + temp + "</p>" +
+       "<img src=\"/test.svg\" />" +
+     "</body>" +
+     "</html>"
+     ;
+  script += (String)hr + min%60 + sec%60 ;  
+  
+  webserver.send(200, "text/html", script);
+  
+  digitalWrite(led, LOW);
+}
+
+
+//----------------------------------------------------------
+void handleNotFound() {
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += webserver.uri();
+  message += "\nMethod: ";
+  message += (webserver.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += webserver.args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < webserver.args(); i++) {
+    message += " " + webserver.argName(i) + ": " + webserver.arg(i) + "\n";
+  }
+
+  webserver.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+
+//----------------------------------------------------------
+void setup() {
+  sprintf(url, "%s:%d/","http://ford.sytes.net",wwwport); 
+  
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  WiFi.config(this_ip, gateway, subnet);   // static IP   
+  
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Connect Failed! Rebooting...");
+    delay(1000);
+    ESP.restart();
+  }
+  ArduinoOTA.begin();
+
+  webserver.on("/", []() {
+    if (!webserver.authenticate(www_username, www_password))
+      //Basic Auth Method with Custom realm and Failure Response
+      //return webserver.requestAuthentication(BASIC_AUTH, www_realm, authFailResponse);
+      //Digest Auth Method with realm="Login Required" and empty Failure Response
+      //return webserver.requestAuthentication(DIGEST_AUTH);
+      //Digest Auth Method with Custom realm and empty Failure Response
+      //return webserver.requestAuthentication(DIGEST_AUTH, www_realm);
+      //Digest Auth Method with Custom realm and Failure Response
+    {
+      return webserver.requestAuthentication(DIGEST_AUTH, www_realm, authFailResponse);
+    }
+
+    handleRoot();
+  });
+  
+  webserver.on("/test.svg", drawGraph);
+  webserver.on("/inline", []() {
+    webserver.send(200, "text/plain", "this works as well");
+  });
+  webserver.onNotFound(handleNotFound);
+  webserver.begin();
+  
+  Serial.println("HTTP webserver started");
+
+  Serial.print("Open http://");
+  Serial.print(WiFi.localIP());
+  Serial.print(":");
+  Serial.print(wwwport);
+  Serial.println("/ in your browser to see it working");
+}
+
+
+//----------------------------------------------------------
+void loop() {
+  ArduinoOTA.handle();
+  webserver.handleClient();
+}
+
+
+//----------------------------------------------------------
+void drawGraph() {
+  String out = "";
+  char temp[100];
+  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
+  out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
+  out += "<g stroke=\"black\">\n";
+  int y = rand() % 130;
+  for (int x = 10; x < 390; x += 10) {
+    int y2 = rand() % 130;
+    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
+    out += temp;
+    y = y2;
+  }
+  out += "</g>\n</svg>\n";
+
+  webserver.send(200, "image/svg+xml", out);
+}
+
+//----------------------------------------------------------
+// EOF
---
+++ ESP32-IoT/WebServer_2LED_OnOff_AdvAuth_002/WebServer_2LED_OnOff_AdvAuth_002.ino
@@ -0,0 +1,240 @@
+// Create A Simple ESP32 Web Server In Arduino IDE
+// https://lastminuteengineers.com/creating-esp32-web-server-arduino-ide/
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <ESPmDNS.h>
+#include <ArduinoOTA.h>
+#include <WebServer.h>
+#include <WiFiServer.h>
+
+
+/*Put your SSID & Password*/
+const char* ssid = "WLAN";  // Enter SSID here
+const char* password = "18658";  //Enter Password here
+
+
+char www_username[64] = "admin";
+char www_password[64] = "esp32";
+
+
+
+
+#define     this_ip_4    202      // <<< static webserver  ip (4th byte)
+IPAddress   this_ip(192, 168, 2, this_ip_4); // <<< static local IP of this ESP-webserver
+IPAddress   gateway(192, 168, 2, 1);         // <<< LAN Gateway IP
+IPAddress   subnet(255, 255, 255, 0);        // <<< LAN Subnet Mask
+
+
+
+#define wwwport 8008
+#define lanport 8081
+WebServer  webserver(wwwport);   // router http port www
+WiFiServer wifiserver(lanport);  // router wifi port lan 
+
+
+
+// allows you to set the realm of authentication Default:"Login Required"
+const char* www_realm = "Custom Auth Realm";
+// the Content of the HTML response in case of Unautherized Access Default:empty
+String authFailResponse = "Authentication Failed";
+
+
+uint8_t LED1pin = LED_BUILTIN;
+bool LED1status = LOW;
+
+uint8_t LED2pin = 12;
+bool LED2status = LOW;
+
+
+
+//----------------------------------------------------------
+void handleNotFound() {
+ 
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += webserver.uri();
+  message += "\nMethod: ";
+  message += (webserver.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += webserver.args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < webserver.args(); i++) {
+    message += " " + webserver.argName(i) + ": " + webserver.arg(i) + "\n";
+  }
+
+  webserver.send(404, "text/plain", message);
+ 
+}
+
+
+
+
+
+//----------------------------------------------------------
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  
+  pinMode(LED1pin, OUTPUT);
+  pinMode(LED2pin, OUTPUT);
+
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+
+  //connect to your local wi-fi network
+  WiFi.begin(ssid, password);
+  WiFi.config(this_ip, gateway, subnet);   // static IP  
+
+  //check wi-fi is connected to wi-fi network
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(1000);
+  Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected..!");
+  Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
+
+
+  ArduinoOTA.begin();
+  webserver.on("/", []() {
+    if (!webserver.authenticate(www_username, www_password))
+      //Basic Auth Method with Custom realm and Failure Response
+      //return webserver.requestAuthentication(BASIC_AUTH, www_realm, authFailResponse);
+      //Digest Auth Method with realm="Login Required" and empty Failure Response
+      //return webserver.requestAuthentication(DIGEST_AUTH);
+      //Digest Auth Method with Custom realm and empty Failure Response
+      //return webserver.requestAuthentication(DIGEST_AUTH, www_realm);
+      //Digest Auth Method with Custom realm and Failure Response
+    {
+      return webserver.requestAuthentication(DIGEST_AUTH, www_realm, authFailResponse);
+    }
+    handle_OnConnect();
+  });
+  
+ /*
+  webserver.on("/inline", []() {
+    webserver.send(200, "text/plain", "this works as well");
+  });
+ */ 
+ 
+  webserver.on("/led1on", handle_led1on);
+  webserver.on("/led1off", handle_led1off);
+  webserver.on("/led2on", handle_led2on);
+  webserver.on("/led2off", handle_led2off);
+  webserver.onNotFound(handle_NotFound);
+
+  webserver.begin();
+  
+  Serial.println("HTTP webserver started");
+}
+
+
+
+//----------------------------------------------------------
+void loop() {
+  ArduinoOTA.handle();
+  
+  webserver.handleClient();
+  if(LED1status)
+  {digitalWrite(LED1pin, HIGH);}
+  else
+  {digitalWrite(LED1pin, LOW);}
+  
+  if(LED2status)
+  {digitalWrite(LED2pin, HIGH);}
+  else
+  {digitalWrite(LED2pin, LOW);}
+}
+
+
+
+//----------------------------------------------------------
+void handle_OnConnect() {
+  LED1status = LOW;
+  LED2status = LOW;
+  Serial.println("GPIO13 Status: OFF | GPIO12 Status: OFF");
+  webserver.send(200, "text/html", SendHTML(LED1status,LED2status)); 
+}
+
+//----------------------------------------------------------
+void handle_led1on() {
+  LED1status = HIGH;
+  Serial.println("GPIO13 Status: ON");
+  webserver.send(200, "text/html", SendHTML(true,LED2status)); 
+}
+
+//----------------------------------------------------------
+void handle_led1off() {
+  LED1status = LOW;
+  Serial.println("GPIO13 Status: OFF");
+  webserver.send(200, "text/html", SendHTML(false,LED2status)); 
+}
+
+//----------------------------------------------------------
+void handle_led2on() {
+  LED2status = HIGH;
+  Serial.println("GPIO12 Status: ON");
+  webserver.send(200, "text/html", SendHTML(LED1status,true)); 
+}
+
+//----------------------------------------------------------
+void handle_led2off() {
+  LED2status = LOW;
+  Serial.println("GPIO12 Status: OFF");
+  webserver.send(200, "text/html", SendHTML(LED1status,false)); 
+}
+
+//----------------------------------------------------------
+void handle_NotFound(){
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += webserver.uri();
+  message += "\nMethod: ";
+  message += (webserver.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += webserver.args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < webserver.args(); i++) {
+    message += " " + webserver.argName(i) + ": " + webserver.arg(i) + "\n";
+  }
+
+  webserver.send(404, "text/plain", message);  
+  //webserver.send(404, "text/plain", "Not found");
+}
+
+//----------------------------------------------------------
+String SendHTML(uint8_t led1stat,uint8_t led2stat){
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<title>LED Control</title>\n";
+  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
+  ptr +=".button {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
+  ptr +=".button-on {background-color: #3498db;}\n";
+  ptr +=".button-on:active {background-color: #2980b9;}\n";
+  ptr +=".button-off {background-color: #34495e;}\n";
+  ptr +=".button-off:active {background-color: #2c3e50;}\n";
+  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<h1>ESP32 Web Server</h1>\n";
+    ptr +="<h3>Using Station(STA) Mode</h3>\n";
+  
+   if(led1stat)
+  {ptr +="<p>LED1 Status: ON</p><a class=\"button button-off\" href=\"/led1off\">OFF</a>\n";}
+  else
+  {ptr +="<p>LED1 Status: OFF</p><a class=\"button button-on\" href=\"/led1on\">ON</a>\n";}
+
+  if(led2stat)
+  {ptr +="<p>LED2 Status: ON</p><a class=\"button button-off\" href=\"/led2off\">OFF</a>\n";}
+  else
+  {ptr +="<p>LED2 Status: OFF</p><a class=\"button button-on\" href=\"/led2on\">ON</a>\n";}
+
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
+}
