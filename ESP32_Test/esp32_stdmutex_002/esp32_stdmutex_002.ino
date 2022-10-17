e20b930757282984d0cce982a9d7c81fecae7feb
dsyleixa <29887867+dsyleixa@users.noreply.github.com>
2022-10-17 16:22:52 +0200

    Update esp32_stdmutex_002.ino

--- ESP32_Test/esp32_stdmutex_002/esp32_stdmutex_002.ino
+++ ESP32_Test/esp32_stdmutex_002/esp32_stdmutex_002.ino
@@ -1,7 +1,4 @@
-another issue can be solved by std lib too: thread-safe r/w communication when using std::mutex.
-e.g., if 1 thread is accessing i2c or Serial() and another parallel thread requires access too, or if different threads read or write to identical global variables or arrays, then simply the thread which came first may reserve access by mutexes, and when finished then simply frees it again: the timeslice scheduler will not muddle that up when switching to the other thread.
-A simple ESP32 std::thread/mutex example how they did it:
-```
+
 
 // std::thread plus mutex for ESP32, Arduino IDE
 
@@ -72,5 +69,5 @@ void loop() {
     main_loop_counter++;
 }
 
-```
+
 
