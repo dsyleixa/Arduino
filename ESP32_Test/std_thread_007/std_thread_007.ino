21f73f9c38c65036a57ee700a7a10d7847a3f726
dsyleixa <29887867+dsyleixa@users.noreply.github.com>
2023-01-14 14:09:35 +0100

    Update std_thread_007.ino

--- ESP32_Test/std_thread_007/std_thread_007.ino
+++ ESP32_Test/std_thread_007/std_thread_007.ino
@@ -139,7 +139,7 @@ void sort_loop() {
 //--------------------------------------------------------------------
 void GPIO_loop() {
 
-   thread_local uint32_t counter = 0, i=0, timerms;
+   thread_local uint32_t counter = 0, timerms;
    vTaskPrioritySet( NULL, ESP_TASK_MAIN_PRIO ); // set Priority = main prio
    
    Serial.println((String)"GPIO_loop Current priority :" + uxTaskPriorityGet(NULL)+"\n");
