9a30ee078798269e205458d4060f92cffc5bc4d6
dsyleixa <29887867+dsyleixa@users.noreply.github.com>
2026-06-29 17:03:24 +0200

    Add files via upload

---
+++ ESP8266-IoT/NodeMCU_client_3_017g/data/settings-Muster.h
@@ -0,0 +1,13 @@
+#ifndef _SETTINGS_H_
+#define _SETTINGS_H_
+
+char* website_pwd       = "abcdefg";
+char* website_title     = "mywebsite";
+char* website_url       = "http://mywebsite.de";
+const char* ssid        = "WLAN-name";
+const char* password    = "123456789";
+char* email_notif       = "ich@gmail.com"
+char* email_emerg       = "Verwandter@gmail.com"
+
+
+#endif
